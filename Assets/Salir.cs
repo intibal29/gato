33c6077a7cf1de42eec94d9d22e6124c95816ade
intibal29 <117351402+intibal29@users.noreply.github.com>
2024-12-09 10:44:20 +0100

    menu

--- Assets/Salir.cs
+++ Assets/Salir.cs
@@ -10,8 +10,6 @@ public class Salir : MonoBehaviour
         // Cierra la aplicación
         Application.Quit();
 
-        // Esto solo funciona fuera del editor. Si quieres probarlo en el editor,
-        // puedes usar Debug.Log o una función condicional para simularlo.
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
